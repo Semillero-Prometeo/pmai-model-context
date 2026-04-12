1a85a92d6ba1884d34ce771b5adbf5814f568a69
Andrea Lopez <andreap-lopezg@unilibre.edu.co>
2026-04-12 17:00:52 -0500

    Refactor the code structure to improve readability and maintainability, and implement the format to start with the metrics already implemented in the model.📈

--- utils/metrics/rendimiento/measure_memory.py
+++ utils/metrics/rendimiento/measure_memory.py
@@ -1,4 +1,5 @@
-
+import psutil
+import psutil
 def measure_memory():
     return psutil.virtual_memory().used / (1024 ** 3)
 
--- utils/metrics/rendimiento/uso_CPU.py
+++ utils/metrics/rendimiento/uso_CPU.py
@@ -1,23 +1,8 @@
+#https://psutil.readthedocs.io/stable/
+
 import psutil
-import time
+import os
 
-
-def medir_uso_cpu():
-    """
-    Mide el uso de CPU por núcleo durante un período de tiempo determinado. Este método proporciona información detallada sobre la carga de trabajo en cada núcleo del procesador, lo que puede ser útil para identificar cuellos de botella y optimizar el rendimiento del modelo.
-    """
-    
-    print("Monitoreando CPU por núcleo...")
-    for i in range(3):
-
-        uso_por_nucleo = psutil.cpu_percent(interval=1, percpu=True)
-        
-        print(f"Uso por núcleo: {uso_por_nucleo}")
-
-        
-        for idx, porcentaje in enumerate(uso_por_nucleo):
-            print(f"  Núcleo {idx}: {porcentaje}%")
-        print("-" * 20)
-        time.sleep(1)  
-        
-    return uso_por_nucleo
+def measure_process_cpu(interval=1):
+    p = psutil.Process(os.getpid())
+    return p.cpu_percent(interval=interval)

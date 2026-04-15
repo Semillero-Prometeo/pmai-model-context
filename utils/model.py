6b115cc4a41e429611d73cd783e5f86e7f054563
anpalop <anpalop@ibm.com>
2026-04-14 22:43:57 -0500

    correccion

--- utils/model.py
+++ utils/model.py
@@ -5,7 +5,10 @@ def import_Blip_model():
     """
     
     """
-    processor = AutoProcessor.from_pretrained("Salesforce/blip-image-captioning-base")
+    try:
+        processor = AutoProcessor.from_pretrained("Salesforce/blip-image-captioning-base", use_fast=True)
+    except ImportError:
+        processor = AutoProcessor.from_pretrained("Salesforce/blip-image-captioning-base")
     model = BlipForConditionalGeneration.from_pretrained("Salesforce/blip-image-captioning-base")
     return processor, model
     
